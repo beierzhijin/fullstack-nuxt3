056a373e4f26578baae9f1d99be6ee0426ddc13c
刘仁钦 <klau.lover@foxmail.com>
2024-08-20 17:30:16 +0800

    feat(layouts)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,23 @@
 <template>
   <!-- https://nuxt.com/docs/guide/directory-structure/layouts -->
   <!-- Unlike other components, your layouts must have a single root element to allow Nuxt to apply transitions between layout changes - and this root element cannot be a <slot />. -->
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Main</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">About</NuxtLink>
-        </li>
-      </ul>
-    </nav>
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">August</NuxtLink>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Main</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <slot></slot>
   </div>
 </template>
