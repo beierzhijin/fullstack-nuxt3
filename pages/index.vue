e56ccd51b73b5114acc5ab58608a84909a7769a8
刘仁钦 <klau.lover@foxmail.com>
2024-08-22 17:18:58 +0800

    feat(seo)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    pages - index
+    welcome to the main page
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: '主页',
+  description: '这里是主页的描述',
+  ogTitle: '[og:title]',
+  ogDescription: '[og:description]',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
